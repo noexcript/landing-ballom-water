85c66e82ce26200e26dd9f36acb13dd0e85e1a2e
Noah Magalhaes <noexcript@gmail.com>
2024-05-04 00:57:35 +0100

    feat: creation of some components like main-content-main sidebar

---
+++ src/components/main-content-main/index.jsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+import React from "react";
+
+function mainContentMain() {
+  return <div className="main-content">
+    
+  </div>;
+}
+
+export default mainContentMain;
---
+++ src/components/main-content/index.jsx
@@ -0,0 +1,8 @@
+import "./styles.css";
+import React from "react";
+
+function MainContent() {
+  return <div className="main-content"></div>;
+}
+
+export default MainContent;
